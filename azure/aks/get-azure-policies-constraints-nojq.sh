3ceff8ebb35e2a66305e32a13ae40506e152eb96
Mathieu Coavoux <mathieu.coavoux@ses.com>
2025-04-18 13:07:55 +0200

    fix

--- azure/aks/get-azure-policies-constraints-nojq.sh
+++ azure/aks/get-azure-policies-constraints-nojq.sh
@@ -5,7 +5,7 @@ get_default() {
   for crd in $(kubectl get crd -l managed-by=azure-policy-addon -o custom-columns="CRD-NAME":.metadata.name --no-headers)
   do
     kind=$(kubectl get crd ${crd} -o custom-columns="KIND":.spec.names.kind --no-headers)
-    kubectl get ${kind} -o custom-columns="kind":.kind,"name":.metadata.name,"enforcement":.spec.enforcementAction,"violations":.status.totalViolation,"assignment-id":.metadata.annotations.azure-policy-assignment-id,"definition-id":.metadata.annotations.azure-policy-definition-id,"setdefinition-id":.metadata.annotations.azure-policy-setdefinition-id --no-headers
+    kubectl get ${kind} -o custom-columns="kind":.kind,"name":.metadata.name,"enforcement":.spec.enforcementAction,"violations":.status.totalViolations,"assignment-id":.metadata.annotations.azure-policy-assignment-id,"definition-id":.metadata.annotations.azure-policy-definition-id,"setdefinition-id":.metadata.annotations.azure-policy-setdefinition-id --no-headers
   done
 }
 
