779c2a07cb588da4b93d99e46ddd38ba460247df
Mathieu Coavoux <mathieu.coavoux@ses.com>
2025-04-18 13:01:18 +0200

    reformat table

--- azure/aks/get-azure-policies-constraints-nojq.sh
+++ azure/aks/get-azure-policies-constraints-nojq.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 get_default() {
+  echo -e "kind\t\tname\t\tenforcement\t\tviolations\t\tassignment-id\t\t\t\t\tdefinition-id\t\t\t\t\tsetdefinition-id"
   for crd in $(kubectl get crd -l managed-by=azure-policy-addon -o custom-columns="CRD-NAME":.metadata.name --no-headers)
   do
     kind=$(kubectl get crd ${crd} -o custom-columns="KIND":.spec.names.kind --no-headers)
-    kubectl get ${kind} -o custom-columns="kind":.kind,"name":.metadata.name,"enforcement":.spec.enforcementAction,"assignment-id":.metadata.annotations.azure-policy-assignment-id,"definition-id":.metadata.annotations.azure-policy-definition-id,"setdefinition-id":.metadata.annotations.azure-policy-setdefinition-id
+    kubectl get ${kind} -o custom-columns="kind":.kind,"name":.metadata.name,"enforcement":.spec.enforcementAction,"violations": .status.totalViolation,"assignment-id":.metadata.annotations.azure-policy-assignment-id,"definition-id":.metadata.annotations.azure-policy-definition-id,"setdefinition-id":.metadata.annotations.azure-policy-setdefinition-id --no-headers
   done
 }
 
